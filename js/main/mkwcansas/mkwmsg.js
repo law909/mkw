c4e04794ebaa3520dbff008fb273311b15c88304
btltx <btltx@btlt2>
2013-09-12 14:47:09 +0200

    Szövegek

--- js/main/mkwcansas/mkwmsg.js
+++ js/main/mkwcansas/mkwmsg.js
@@ -7,7 +7,7 @@ var mkwmsg = {
 	KosarMennyisegNulla: 'Ha nem kíván vásárolni a termékből, törölje a "Töröl" gombbal.',
 	ChkRegLogin: 'Válassza ki, hogy szeretne-e regisztrálni a vásárláshoz, vagy jelentkezzen be!',
 	ChkASZF: 'Megrendelés előtt kérjük, fogadja el az ÁSZF-et. Ehhez kattintson az "Elfogadom az ÁSZF-et" sorra.',
-	ChkKosarValtozott: 'A kosár tartalma megrendelés közben megváltozott, kérem ellenőrizze.',
+	ChkKosarValtozott: 'A kosár tartalma időközben megváltozott, kérem ellenőrizze.',
 	ChkKosarUres: 'Az Ön kosara üres.',
 	FiokAdataitModositjuk: 'Adatait módosítjuk...',
 	DialogFejlec: 'Értesítés',
