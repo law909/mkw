b57a6b04a3958c7ebacd41443fe80dfebd252d55
law909 <law@mattkft.hu>
2013-11-06 11:32:55 +0100

    mkwmailer

---
+++ mkw/mkwmailer.php
@@ -0,0 +1,36 @@
+<?php
+namespace mkw;
+
+class mkwMailer {
+    private $to;
+    private $subject;
+    private $message;
+
+    public function setTo($to) {
+        $this->to = $to;
+    }
+
+    public function getTo() {
+        return $this->to;
+    }
+
+    public function setSubject($param) {
+        $this->subject = $param;
+    }
+
+    public function getSubject() {
+        return $this->subject;
+    }
+
+    public function setMessage($message) {
+        $this->message = $message;
+    }
+
+    public function getMessage() {
+        return $this->message;
+    }
+
+    public function send() {
+        return mail($this->to, $this->subject, $this->message);
+    }
+}
