ef83954cb51d848335c0c37245227028a9e78f95
law909 <law@mattkft.hu>
2013-12-15 20:26:39 +0100

    bizonylat statuszvaltas 1

--- js/admin/default/emailtemplate.js
+++ js/admin/default/emailtemplate.js
@@ -1,50 +1,50 @@
-$(document).ready(function(){
-	var emailtemplate={
-		container:'#mattkarb',
-		viewUrl:'/admin/emailtemplate/getkarb',
-		newWindowUrl:'/admin/emailtemplate/viewkarb',
-		saveUrl:'/admin/emailtemplate/save',
-		beforeShow:function() {
-			if (!$.browser.mobile) {
+$(document).ready(function() {
+    var emailtemplate = {
+        container: '#mattkarb',
+        viewUrl: '/admin/emailtemplate/getkarb',
+        newWindowUrl: '/admin/emailtemplate/viewkarb',
+        saveUrl: '/admin/emailtemplate/save',
+        beforeShow: function() {
+            if (!$.browser.mobile) {
 //				CKFinder.setupCKEditor( null, '/ckfinder/' );
 //				$('#LeirasEdit').ckeditor();
-			}
-		},
-		beforeHide:function() {
-			if (!$.browser.mobile) {
-				editor=$('#LeirasEdit').ckeditorGet();
-				if (editor) {
-					editor.destroy();
-				}
-			}
-		},
-		onSubmit:function() {
-			$('#messagecenter')
-				.html('A mentés sikerült.')
-				.hide()
-				.addClass('matt-messagecenter ui-widget ui-state-highlight')
-				.one('click',messagecenterclick)
-				.slideToggle('slow');
-		}
-	}
+            }
+        },
+        beforeHide: function() {
+            if (!$.browser.mobile) {
+//				editor = $('#LeirasEdit').ckeditorGet();
+//				if (editor) {
+//					editor.destroy();
+//				}
+            }
+        },
+        onSubmit: function() {
+            $('#messagecenter')
+                    .html('A mentés sikerült.')
+                    .hide()
+                    .addClass('matt-messagecenter ui-widget ui-state-highlight')
+                    .one('click', messagecenterclick)
+                    .slideToggle('slow');
+        }
+    }
 
-	if ($.fn.mattable) {
-		$('#mattable-select').mattable({
-			filter:{
-				fields:['#nevfilter']
-			},
-			tablebody:{
-				url:'/admin/emailtemplate/getlistbody'
-			},
-			karb:emailtemplate
-		});
-		$('.js-maincheckbox').change(function(){
-			$('.js-egyedcheckbox').prop('checked',$(this).prop('checked'));
-		});
-	}
-	else {
-		if ($.fn.mattkarb) {
-			$('#mattkarb').mattkarb($.extend({},emailtemplate,{independent:true}));
-		}
-	}
+    if ($.fn.mattable) {
+        $('#mattable-select').mattable({
+            filter: {
+                fields: ['#nevfilter']
+            },
+            tablebody: {
+                url: '/admin/emailtemplate/getlistbody'
+            },
+            karb: emailtemplate
+        });
+        $('.js-maincheckbox').change(function() {
+            $('.js-egyedcheckbox').prop('checked', $(this).prop('checked'));
+        });
+    }
+    else {
+        if ($.fn.mattkarb) {
+            $('#mattkarb').mattkarb($.extend({}, emailtemplate, {independent: true}));
+        }
+    }
 });
