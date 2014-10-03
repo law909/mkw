69f4f3dbc4e34b9208221ff80303b3cd755d1d6a
law909 <balint.lovey@gmail.com>
2014-10-04 00:06:38 +0200

    karbantartas

---
+++ karbantartas.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <div style="margin-left: auto;margin-right: auto;width: 724px;">
+        <img src="themes/main/mkwcansas/img/karbantartas.png">
+        </div>
+    </body>
+</html>
+
