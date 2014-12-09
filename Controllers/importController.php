08f4c4c56710e13ed169a25956cc621d9815df8d
law909 <balint.lovey@gmail.com>
2014-12-09 16:12:57 +0100

    statuszemail -

--- Controllers/importController.php
+++ Controllers/importController.php
@@ -1120,11 +1120,13 @@ class importController extends \mkwhelpers\Controller {
                     store::getEm()->persist($fej);
                     store::getEm()->flush();
                 }
-                $statusz = $fej->getBizonylatstatusz();
+/**                $statusz = $fej->getBizonylatstatusz();
                 if ($statusz) {
                     $emailtpl = $statusz->getEmailtemplate();
                     $fej->sendStatuszEmail($emailtpl, null, false);
                 }
+ *
+ */
             }
 
         }
