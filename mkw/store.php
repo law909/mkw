5f69cb1f4fe72c1e95eee553451a568653fc1b79
law909 <balint.lovey@gmail.com>
2014-12-09 14:46:42 +0100

    client ip logging

--- mkw/store.php
+++ mkw/store.php
@@ -28,20 +28,37 @@ class Store {
 
     public static function getClientIp() {
         $ipaddress = '';
-        if ($_SERVER['HTTP_CLIENT_IP'])
+        if (array_key_exists('HTTP_CLIENT_IP', $_SERVER) && $_SERVER['HTTP_CLIENT_IP']) {
             $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        else if ($_SERVER['HTTP_X_FORWARDED_FOR'])
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        else if ($_SERVER['HTTP_X_FORWARDED'])
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        else if ($_SERVER['HTTP_FORWARDED_FOR'])
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        else if ($_SERVER['HTTP_FORWARDED'])
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        else if ($_SERVER['REMOTE_ADDR'])
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
-        else
-            $ipaddress = 'UNKNOWN';
+        }
+        else {
+            if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && $_SERVER['HTTP_X_FORWARDED_FOR']) {
+                $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+            }
+            else {
+                if (array_key_exists('HTTP_X_FORWARDED', $_SERVER) && $_SERVER['HTTP_X_FORWARDED']) {
+                    $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+                }
+                else {
+                    if (array_key_exists('HTTP_FORWARDED_FOR', $_SERVER) && $_SERVER['HTTP_FORWARDED_FOR']) {
+                        $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+                    }
+                    else {
+                        if (array_key_exists('HTTP_FORWARDED', $_SERVER) && $_SERVER['HTTP_FORWARDED']) {
+                            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+                        }
+                        else {
+                            if (array_key_exists('REMOTE_ADDR', $_SERVER) && $_SERVER['REMOTE_ADDR']) {
+                                $ipaddress = $_SERVER['REMOTE_ADDR'];
+                            }
+                            else {
+                                $ipaddress = 'UNKNOWN';
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return $ipaddress;
     }
 
