97543737fdd32448f1da556de39edcccf6fcc840
law909 <balint.lovey@gmail.com>
2015-11-25 14:24:29 +0100

    bank dialogcenter

--- js/admin/default/bankbizonylat.js
+++ js/admin/default/bankbizonylat.js
@@ -16,7 +16,8 @@ $(document).ready(function () {
         newWindowUrl: '/admin/bankbizonylatfej/viewkarb',
         saveUrl: '/admin/bankbizonylatfej/save',
         beforeShow: function () {
-            var keltedit = $('#KeltEdit');
+            var keltedit = $('#KeltEdit'),
+                dialogcenter = $('#dialogcenter');
             keltedit.datepicker($.datepicker.regional['hu']);
             keltedit.datepicker('option', 'dateFormat', 'yy.mm.dd');
             keltedit.datepicker('setDate', keltedit.attr('data-datum'));
