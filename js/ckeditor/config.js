2dd5cfd1a37232ed94e10b9bbb04b0477f48ffbc
law909 <balint.lovey@gmail.com>
2016-09-20 15:33:20 +0200

    CKEditor

---
+++ js/ckeditor/config.js
@@ -0,0 +1,29 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+config.toolbar = 'SIIKerES';
+
+config.toolbar_SIIKerES =
+[
+	{ name: 'document', items : [ 'Source','-','DocProps','Print','-','Templates' ] },
+	{ name: 'clipboard', items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+	{ name: 'editing', items : [ 'Find','Replace','-','SelectAll','-','SpellChecker', 'Scayt' ] },
+	{ name: 'forms', items : [ 'Form', 'Checkbox', 'Radio', 'TextField', 'Textarea', 'Select', 'Button', 'ImageButton', 'HiddenField' ] },
+	'/',
+	{ name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+	{ name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
+	{ name: 'links', items : [ 'Link','Unlink','Anchor' ] },
+	{ name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+	'/',
+	{ name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
+	{ name: 'colors', items : [ 'TextColor','BGColor' ] },
+	{ name: 'tools', items : [ 'Maximize', 'ShowBlocks','-','About' ] }
+];
+config.language = 'hu';
+config.protectedSource.push(/{[\s\S]*?}/g);
+	// config.uiColor = '#AADC6E';
+};
