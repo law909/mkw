808b8eefc906e55a8b7a37a364b833fc30c3b340
law909 <balint.lovey@gmail.com>
2016-12-22 22:39:29 +0100

    EAN13 checksum

--- mkw/store.php
+++ mkw/store.php
@@ -962,4 +962,18 @@ class store {
         return mb_convert_encoding($mit, 'UTF8', 'ISO-8859-2');
     }
 
+    public static function generateEAN13($number) {
+        $code = '200' . str_pad($number, 9, '0');
+        $weightflag = true;
+        $sum = 0;
+        // Weight for a digit in the checksum is 3, 1, 3.. starting from the last digit.
+        // loop backwards to make the loop length-agnostic. The same basic functionality
+        // will work for codes of different lengths.
+        for ($i = strlen($code) - 1; $i >= 0; $i--) {
+            $sum += (int)$code[$i] * ($weightflag ? 3 : 1);
+            $weightflag = !$weightflag;
+        }
+        $code .= (10 - ($sum % 10)) % 10;
+        return $code;
+    }
 }
