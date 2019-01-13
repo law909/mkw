7eeed2ce63fa2e2b00280a2cd1751ba61ac85a7d
law909 <balint.lovey@gmail.com>
2019-01-14 00:33:27 +0100

    blog

--- versions/0026.php
+++ versions/0026.php
@@ -3,7 +3,7 @@ function run() {
 
     runsql('INSERT INTO menu (menucsoport_id, nev, url, routename, jogosultsag, lathato, sorrend, class)'
         . ' VALUES '
-        . '(1, "Blogposztok","/admin/blogposzt/viewlist","/admin/blogposzt",20,0,350, "")'
+        . '(3, "Blogposztok","/admin/blogposzt/viewlist","/admin/blogposzt",20,1,350, "")'
     );
 
 }
