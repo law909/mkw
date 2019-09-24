06f1bb0678b712a880d92a290e3fcf651a61e639
law909 <balint.lovey@gmail.com>
2019-09-24 14:39:11 +0200

    leltartetel order

--- Entities/LeltartetelRepository.php
+++ Entities/LeltartetelRepository.php
@@ -12,7 +12,7 @@ class LeltartetelRepository extends \mkwhelpers\Repository {
         ));
     }
 
-    public function getWithJoins($filter, $order, $offset = 0, $elemcount = 0) {
+    public function getWithJoins($filter, $order = array(), $offset = 0, $elemcount = 0) {
         $q = $this->_em->createQuery('SELECT _xx,t,tv'
             . ' FROM Entities\Leltartetel _xx'
             . ' LEFT JOIN _xx.termek t'
