7a1f438dc37f014a6432b017a87fa7be34502cd0
law909 <balint.lovey@gmail.com>
2020-11-08 00:14:39 +0100

    pubadmin ora valasztas

--- Controllers/pubadminController.php
+++ Controllers/pubadminController.php
@@ -12,6 +12,37 @@ class pubadminController extends mkwhelpers\Controller {
         $view = $this->createPubAdminView('main.tpl');
         $view->setVar('pagetitle', t('Főoldal'));
 
+        $dolgozo = $this->getRepo('Entities\Dolgozo')->find(\mkw\store::getPubAdminSession()->pk);
+        if ($dolgozo) {
+            $mainap = new \DateTime();
+            $filter = new \mkwhelpers\FilterDescriptor();
+            $filter->addFilter('dolgozo', '=', $dolgozo);
+            $filter->addFilter('nap', '=', $mainap->format('N'));
+            $filter->addFilter('inaktiv', '=', false);
+            $orak = $this->getRepo('Entities\Orarend')->getAll($filter);
+            $oralista = [];
+            /** @var Entities\Orarend $ora */
+            foreach ($orak as $ora) {
+                $oralista[] = [
+                    'id' => $ora->getId(),
+                    'nev' => $ora->getKezdetStr() . ' ' . $ora->getNev() . ' (' . $ora->getNapNev() . ')'
+                ];
+            }
+            $filter->clear();
+            $filter->addFilter('helyettesito', '=', $dolgozo);
+            $filter->addFilter('datum', '=', $mainap);
+            $filter->addFilter('inaktiv', '=', false);
+            $helyettek = $this->getRepo('Entities\Orarendhelyettesites')->getAll($filter);
+            /** @var Entities\Orarendhelyettesites $helyett*/
+            foreach ($helyettek as $helyett) {
+                $oralista[] = [
+                    'id' => $helyett->getId(),
+                    'nev' => $helyett->getOrarendNev()
+                ];
+            }
+            $view->setVar('oralista', $oralista);
+        }
+
         $view->printTemplateResult();
     }
 
--- tpl/pubadmin/darshan/main.tpl
+++ tpl/pubadmin/darshan/main.tpl
@@ -6,4 +6,18 @@
         <a class="btn btn-darshan" href="/pubadmin/logout">Kijelentkezés</a>
     </div>
 
+    <div class="row">
+        <form class="col">
+            <div class="form-group">
+                <label for="oraselect">Óra</label>
+                <select id="oraselect" class="form-control" name="ora">
+                    <option value="">válassz</option>
+                    {foreach $oralista as $ora}
+                        <option value="{$ora.id}">{$ora.nev}</option>
+                    {/foreach}
+                </select>
+            </div>
+        </form>
+    </div>
+
 {/block}
