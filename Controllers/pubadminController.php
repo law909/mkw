e28ec962e18dd57dc5a147921b5d01983c48c071
law909 <balint.lovey@gmail.com>
2020-11-28 21:31:55 +0100

    pubadmin berlet email

--- Controllers/pubadminController.php
+++ Controllers/pubadminController.php
@@ -192,7 +192,29 @@ class pubadminController extends mkwhelpers\Controller {
                 $this->getEm()->flush();
             }
             if ($rv->isKesobbfizet()) {
-                $berlet->sendEmail(\mkw\store::getParameter(\mkw\consts::JogaBerletFelszolitoSablon));
+                if ($berlet) {
+                    $berlet->sendEmail(\mkw\store::getParameter(\mkw\consts::JogaBerletFelszolitoSablon));
+                }
+                elseif ( \mkw\store::isSendableEmail($rv->getPartneremail())) {
+                    $emailtpl = $this->getRepo('Entities\Emailtemplate')->find(\mkw\store::getParameter(\mkw\consts::JogaBerletFelszolitoSablon));
+                    if ($emailtpl) {
+
+                        $subject = \mkw\store::getTemplateFactory()->createMainView('string:' . $emailtpl->getTargy());
+                        $body = \mkw\store::getTemplateFactory()->createMainView('string:' . str_replace('&#39;', '\'', html_entity_decode($emailtpl->getHTMLSzoveg())));
+                        $body->setVar('partnernev', $rv->getPartnernev());
+                        $body->setVar('datum', date(\mkw\store::$DateFormat));
+                        $body->setVar('berlet', $tipusnev);
+                        $body->setVar('ar', $price);
+
+                        $mailer = \mkw\store::getMailer();
+
+                        $mailer->addTo($rv->getPartneremail());
+                        $mailer->setSubject($subject->getTemplateResult());
+                        $mailer->setMessage($body->getTemplateResult());
+
+                        $mailer->send();
+                    }
+                }
             }
         }
     }
