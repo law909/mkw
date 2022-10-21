0d745a3f8f4a746f63a3a89607b21a23c1fbb805
law909 <balint.lovey@gmail.com>
2022-10-21 06:09:27 +0200

    rating format

--- Controllers/termekertekelesController.php
+++ Controllers/termekertekelesController.php
@@ -5,34 +5,31 @@ namespace Controllers;
 use Entities\Partner;
 use Entities\Termek;
 use Entities\TermekErtekeles;
-use Entities\TermekValtozat,
-	Entities\TermekRecept;
-use mkw\store;
-use mkwhelpers\FilterDescriptor;
-use PhpOffice\PhpSpreadsheet\IOFactory;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
-class termekertekelesController extends \mkwhelpers\MattableController {
+class termekertekelesController extends \mkwhelpers\MattableController
+{
 
-    public function __construct($params) {
-		$this->setEntityName(TermekErtekeles::class);
-		$this->setKarbFormTplName('termekertekeleskarbform.tpl');
-		$this->setKarbTplName('termekertekeleskarb.tpl');
-		$this->setListBodyRowTplName('termekertekeleslista_tbody_tr.tpl');
+    public function __construct($params)
+    {
+        $this->setEntityName(TermekErtekeles::class);
+        $this->setKarbFormTplName('termekertekeleskarbform.tpl');
+        $this->setKarbTplName('termekertekeleskarb.tpl');
+        $this->setListBodyRowTplName('termekertekeleslista_tbody_tr.tpl');
         $this->setListBodyRowVarName('_egyed');
-		parent::__construct($params);
-	}
+        parent::__construct($params);
+    }
 
-	protected function loadVars($t, $forKarb = false) {
-		$x = array();
-		if (!$t) {
-			$t = new \Entities\TermekErtekeles();
-			$this->getEm()->detach($t);
-		}
-		$x['id'] = $t->getId();
-		$x['szoveg'] = $t->getSzoveg();
-		$x['elony'] = $t->getElony();
-		$x['hatrany'] = $t->getHatrany();
+    protected function loadVars($t, $forKarb = false)
+    {
+        $x = array();
+        if (!$t) {
+            $t = new \Entities\TermekErtekeles();
+            $this->getEm()->detach($t);
+        }
+        $x['id'] = $t->getId();
+        $x['szoveg'] = $t->getSzoveg();
+        $x['elony'] = $t->getElony();
+        $x['hatrany'] = $t->getHatrany();
         $x['valasz'] = $t->getValasz();
         $x['ertekeles'] = $t->getErtekeles();
         $x['created'] = $t->getCreatedStr();
@@ -40,74 +37,80 @@ class termekertekelesController extends \mkwhelpers\MattableController {
         $x['partnernev'] = $t->getPartnerNev();
         $x['termek'] = $t->getTermekId();
         $x['termeknev'] = $t->getTermekNev();
-		return $x;
-	}
+        return $x;
+    }
 
     /**
      * @param \Entities\TermekErtekeles $obj
      * @return mixed
      */
-	protected function setFields($obj) {
-		$ck = \mkw\store::getEm()->getRepository(Partner::class)->find($this->params->getIntRequestParam('partner'));
-		if ($ck) {
-			$obj->setPartner($ck);
-		}
-        else {
+    protected function setFields($obj)
+    {
+        $ck = \mkw\store::getEm()->getRepository(Partner::class)->find($this->params->getIntRequestParam('partner'));
+        if ($ck) {
+            $obj->setPartner($ck);
+        } else {
             $obj->setPartner(null);
         }
         $ck = \mkw\store::getEm()->getRepository(Termek::class)->find($this->params->getIntRequestParam('termek'));
         if ($ck) {
             $obj->setTermek($ck);
-        }
-        else {
+        } else {
             $obj->setTermek(null);
         }
-		$obj->setSzoveg($this->params->getStringRequestParam('szoveg'));
+        $obj->setSzoveg($this->params->getStringRequestParam('szoveg'));
         $obj->setElony($this->params->getStringRequestParam('elony'));
         $obj->setHatrany($this->params->getStringRequestParam('hatrany'));
         $obj->setValasz($this->params->getStringRequestParam('valasz'));
         $obj->setErtekeles($this->params->getIntRequestParam('ertekeles'));
 
-		return $obj;
-	}
+        return $obj;
+    }
 
-	public function getlistbody() {
-		$view = $this->createView('termekertekeleslista_tbody.tpl');
+    public function getlistbody()
+    {
+        $view = $this->createView('termekertekeleslista_tbody.tpl');
 
-		$filter = new \mkwhelpers\FilterDescriptor();
+        $filter = new \mkwhelpers\FilterDescriptor();
         if (!is_null($this->params->getRequestParam('partnerfilter', null))) {
-            $filter->addFilter('partner' , '=', $this->params->getIntRequestParam('partnerfilter'));
+            $filter->addFilter('partner', '=', $this->params->getIntRequestParam('partnerfilter'));
         }
 
         $this->initPager($this->getRepo()->getCount($filter));
         $egyedek = $this->getRepo()->getWithJoins(
-            $filter, $this->getOrderArray(), $this->getPager()->getOffset(), $this->getPager()->getElemPerPage());
+            $filter,
+            $this->getOrderArray(),
+            $this->getPager()->getOffset(),
+            $this->getPager()->getElemPerPage()
+        );
 
         echo json_encode($this->loadDataToView($egyedek, 'termekertekeleslista', $view));
-	}
+    }
 
-	public function viewlist() {
-		$view = $this->createView('termekertekeleslista.tpl');
-		$view->setVar('pagetitle', t('Termék értékelések'));
-		$view->setVar('orderselect', $this->getRepo()->getOrdersForTpl());
-		$view->setVar('batchesselect', $this->getRepo()->getBatchesForTpl());
+    public function viewlist()
+    {
+        $view = $this->createView('termekertekeleslista.tpl');
+        $view->setVar('pagetitle', t('Termék értékelések'));
+        $view->setVar('orderselect', $this->getRepo()->getOrdersForTpl());
+        $view->setVar('batchesselect', $this->getRepo()->getBatchesForTpl());
         $partner = new partnerController($this->params);
         $view->setVar('partnerlist', $partner->getSelectList(0));
-		$tcs = new termekController($this->params);
-		$view->setVar('termeklist', $tcs->getSelectList());
-		$view->printTemplateResult();
-	}
+        $tcs = new termekController($this->params);
+        $view->setVar('termeklist', $tcs->getSelectList());
+        $view->printTemplateResult();
+    }
 
-	protected function _getkarb($tplname) {
-		$id = $this->params->getRequestParam('id', 0);
-		$oper = $this->params->getRequestParam('oper', '');
-		$view = $this->createView($tplname);
-		$view->setVar('pagetitle', t('Termék értékelés'));
-		$view->setVar('oper', $oper);
+    protected function _getkarb($tplname)
+    {
+        $id = $this->params->getRequestParam('id', 0);
+        $oper = $this->params->getRequestParam('oper', '');
+        $view = $this->createView($tplname);
+        $view->setVar('pagetitle', t('Termék értékelés'));
+        $view->setVar('oper', $oper);
 
         /** @var TermekErtekeles $te */
-		$te = $this->getRepo()->findWithJoins($id);
-		$view->setVar('egyed', $this->loadVars($te, true));
+        $te = $this->getRepo()->findWithJoins($id);
+        $view->setVar('egyed', $this->loadVars($te, true));
 
         $partner = new partnerController($this->params);
         $view->setVar('partnerlist', $partner->getSelectList(($te ? $te->getPartnerId() : 0)));
@@ -116,6 +119,6 @@ class termekertekelesController extends \mkwhelpers\MattableController {
         $view->setVar('termeklist', $csoport->getSelectList(($te ? $te->getTermekId() : 0)));
 
         $view->printTemplateResult();
-	}
+    }
 
 }
