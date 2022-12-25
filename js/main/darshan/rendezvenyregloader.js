0d35a62afb8ff1bb3cf77df6754304b13c2db8a5
law909 <balint.lovey@gmail.com>
2022-12-25 23:05:02 +0100

    iframe loader

--- js/main/darshan/rendezvenyregloader.js
+++ js/main/darshan/rendezvenyregloader.js
@@ -1,7 +1,8 @@
 
-import billyloader from "./billyloader.js";
-
 const me = new URL(document.currentScript.src);
+
+let billyloader = await import(me.origin + "/js/main/darshan/billyloader.js");
+
 new billyloader(
     'rendezvenyregframe' + me.searchParams.get('i'),
     me.origin,
