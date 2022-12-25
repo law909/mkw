46f32e37982f488350193d4e86d3141a2683d99e
law909 <balint.lovey@gmail.com>
2022-12-25 22:46:06 +0100

    regform

---
+++ js/main/darshan/billyloader.js
@@ -0,0 +1,39 @@
+export default class {
+
+    constructor(frameid, baseurl, params) {
+
+        const me = new URL(document.currentScript.src);
+
+        this.baseUrl = me.origin;
+        this.params = new URLSearchParams(me.search);
+        this.iFrameId = frameid + this.params.get('i');
+
+        console.log('init: ' + this.iFrameId);
+
+        let iframe = document.createElement("iframe");
+        if (iframe.attachEvent) {
+            iframe.attachEvent("onload", () => {
+                this.IFrameOnLoad()
+            })
+        } else {
+            iframe.addEventListener("load", () => {
+                this.IFrameOnLoad()
+            }, false)
+        }
+        iframe.setAttribute('src', new URL(this.baseUrl + '/rendezveny/reg?' + this.params.toString()));
+        iframe.setAttribute('id', this.iFrameId);
+        iframe.setAttribute('height', '100%');
+        iframe.setAttribute('width', '100%');
+        iframe.style.overflow = 'hidden';
+        iframe.style.border = 'none';
+        document.currentScript.parentNode.appendChild(iframe);
+        setTimeout(function () {
+        }, 1e3)
+    }
+
+    IFrameOnLoad() {
+        console.log('iframeonload: ' + this.iFrameId);
+
+        iFrameResize({ log: true }, '#' + this.iFrameId);
+    }
+}
