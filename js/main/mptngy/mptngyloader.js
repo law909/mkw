e54bfb19f2cf53a4799d16477fdd0b64f3552ee6
law909 <balint.lovey@gmail.com>
2023-01-06 09:56:25 +0100

    mptngy pub

---
+++ js/main/mptngy/mptngyloader.js
@@ -0,0 +1,10 @@
+(() => {
+    const me = new URL(document.currentScript.src);
+
+    new billyloader(
+        'mainframe' + me.searchParams.get('i'),
+        me.origin,
+        '/login?',
+        me.searchParams
+    );
+})();
