0b40cc09d27af71b9b6af39ae88ee6b6da75c2a4
law909 <balint.lovey@gmail.com>
2023-02-24 13:19:12 +0100

    mgr login

--- Entities/PartnerRepository.php
+++ Entities/PartnerRepository.php
@@ -4,17 +4,19 @@ namespace Entities;
 
 use Doctrine\ORM\Query\ResultSetMapping;
 
-class PartnerRepository extends \mkwhelpers\Repository {
+class PartnerRepository extends \mkwhelpers\Repository
+{
 
-	public function __construct($em, \Doctrine\ORM\Mapping\ClassMetadata $class) {
-		parent::__construct($em, $class);
-		$this->setEntityname('Entities\Partner');
-		$this->setOrders(array(
-			'1' => array('caption' => 'név szerint növekvő', 'order' => array('nev' => 'ASC')),
-			'2' => array('caption' => 'cím szerint növekvő', 'order' => array('irszam' => 'ASC', 'varos' => 'ASC', 'utca' => 'ASC'))
-		));
+    public function __construct($em, \Doctrine\ORM\Mapping\ClassMetadata $class)
+    {
+        parent::__construct($em, $class);
+        $this->setEntityname('Entities\Partner');
+        $this->setOrders([
+            '1' => ['caption' => 'név szerint növekvő', 'order' => ['nev' => 'ASC']],
+            '2' => ['caption' => 'cím szerint növekvő', 'order' => ['irszam' => 'ASC', 'varos' => 'ASC', 'utca' => 'ASC']]
+        ]);
 
-        $btch = array();
+        $btch = [];
         if (\mkw\store::isMIJSZ()) {
             $btch['mijszexportin'] = 'Oktató export IN';
             $btch['mijszexportus'] = 'Oktató export US';
@@ -24,87 +26,98 @@ class PartnerRepository extends \mkwhelpers\Repository {
         $btch['roadrecordexport'] = 'Roadrecord export';
         $btch['arsavcsere'] = 'Ársáv csere';
         $btch['termekcsoportkedvezmenyedit'] = 'Termékcsoport kedvezmény módosítás';
-		$this->setBatches($btch);
-	}
+        $this->setBatches($btch);
+    }
 
-    public function getSzamlatipusList($sel) {
-        return array(
-            array(
+    public function getSzamlatipusList($sel)
+    {
+        return [
+            [
                 'id' => 0,
                 'caption' => 'magyar',
                 'selected' => ($sel == 0)
-            ),
-            array(
+            ],
+            [
                 'id' => 1,
                 'caption' => 'EU-n belüli',
                 'selected' => ($sel == 1)
-            ),
-            array(
+            ],
+            [
                 'id' => 2,
                 'caption' => 'EU-n kívüli',
                 'selected' => ($sel == 2)
-            )
-        );
+            ]
+        ];
     }
 
-    public function getVatstatusList($sel) {
-	    return array(
-	        array(
-	            'id' => 1,
+    public function getVatstatusList($sel)
+    {
+        return [
+            [
+                'id' => 1,
                 'caption' => 'Belföldi adóalany',
                 'selected' => ($sel == 1)
-            ),
-            array(
+            ],
+            [
                 'id' => 2,
                 'caption' => 'Magánszemély',
                 'selected' => ($sel == 2)
-            ),
-            array(
+            ],
+            [
                 'id' => 3,
                 'caption' => 'Egyéb',
                 'selected' => ($sel == 3)
-            ),
-        );
+            ],
+        ];
     }
 
-	public function getWithJoins($filter, $order, $offset = 0, $elemcount = 0) {
-		$a = $this->alias;
-		$q = $this->_em->createQuery('SELECT ' . $a . ',fm,uk'
-				. ' FROM ' . $this->entityname . ' ' . $a
-				. ' LEFT JOIN ' . $a . '.fizmod fm'
-				. ' LEFT JOIN ' . $a . '.uzletkoto uk '
-				. ' LEFT JOIN ' . $a . '.valutanem v '
-				. ' LEFT JOIN ' . $a . '.szallitasimod szm '
-				. $this->getFilterString($filter)
-				. $this->getOrderString($order));
-		$q->setParameters($this->getQueryParameters($filter));
-		if ($offset > 0) {
-			$q->setFirstResult($offset);
-		}
-		if ($elemcount > 0) {
-			$q->setMaxResults($elemcount);
-		}
-		return $q->getResult();
-	}
-
-	public function getCount($filter) {
-		$a = $this->alias;
-		$q = $this->_em->createQuery('SELECT COUNT(' . $a . ') FROM ' . $this->entityname . ' ' . $a
-				. ' LEFT JOIN ' . $a . '.fizmod fm'
-				. ' LEFT JOIN ' . $a . '.uzletkoto uk '
-				. ' LEFT JOIN ' . $a . '.valutanem v '
-				. ' LEFT JOIN ' . $a . '.szallitasimod szm '
-				. $this->getFilterString($filter));
-		$q->setParameters($this->getQueryParameters($filter));
-		return $q->getSingleScalarResult();
-	}
-
-    public function getAllForSelectList($filter, $order, $offset = 0, $elemcount = 0) {
+    public function getWithJoins($filter, $order, $offset = 0, $elemcount = 0)
+    {
         $a = $this->alias;
-        $q = $this->_em->createQuery('SELECT ' . $a . '.id,' . $a . '.nev, ' . $a . '.irszam, ' . $a . '.varos, ' . $a . '.utca, ' .$a . '.hazszam,' . $a . '.email'
-                . ' FROM ' . $this->entityname . ' ' . $a
-                . $this->getFilterString($filter)
-                . $this->getOrderString($order));
+        $q = $this->_em->createQuery(
+            'SELECT ' . $a . ',fm,uk'
+            . ' FROM ' . $this->entityname . ' ' . $a
+            . ' LEFT JOIN ' . $a . '.fizmod fm'
+            . ' LEFT JOIN ' . $a . '.uzletkoto uk '
+            . ' LEFT JOIN ' . $a . '.valutanem v '
+            . ' LEFT JOIN ' . $a . '.szallitasimod szm '
+            . $this->getFilterString($filter)
+            . $this->getOrderString($order)
+        );
+        $q->setParameters($this->getQueryParameters($filter));
+        if ($offset > 0) {
+            $q->setFirstResult($offset);
+        }
+        if ($elemcount > 0) {
+            $q->setMaxResults($elemcount);
+        }
+        return $q->getResult();
+    }
+
+    public function getCount($filter)
+    {
+        $a = $this->alias;
+        $q = $this->_em->createQuery(
+            'SELECT COUNT(' . $a . ') FROM ' . $this->entityname . ' ' . $a
+            . ' LEFT JOIN ' . $a . '.fizmod fm'
+            . ' LEFT JOIN ' . $a . '.uzletkoto uk '
+            . ' LEFT JOIN ' . $a . '.valutanem v '
+            . ' LEFT JOIN ' . $a . '.szallitasimod szm '
+            . $this->getFilterString($filter)
+        );
+        $q->setParameters($this->getQueryParameters($filter));
+        return $q->getSingleScalarResult();
+    }
+
+    public function getAllForSelectList($filter, $order, $offset = 0, $elemcount = 0)
+    {
+        $a = $this->alias;
+        $q = $this->_em->createQuery(
+            'SELECT ' . $a . '.id,' . $a . '.nev, ' . $a . '.irszam, ' . $a . '.varos, ' . $a . '.utca, ' . $a . '.hazszam,' . $a . '.email'
+            . ' FROM ' . $this->entityname . ' ' . $a
+            . $this->getFilterString($filter)
+            . $this->getOrderString($order)
+        );
         $q->setParameters($this->getQueryParameters($filter));
         if ($offset > 0) {
             $q->setFirstResult($offset);
@@ -115,97 +128,106 @@ class PartnerRepository extends \mkwhelpers\Repository {
         return $q->getScalarResult();
     }
 
-    public function getBizonylatfejLista($keresett) {
+    public function getBizonylatfejLista($keresett)
+    {
         $filter = new \mkwhelpers\FilterDescriptor();
         $filter->addFilter('inaktiv', '=', false);
-        $filter->addFilter(array('_xx.nev', '_xx.email'), 'LIKE', '%' . $keresett . '%');
-        $order = array('_xx.nev' => 'ASC');
+        $filter->addFilter(['_xx.nev', '_xx.email'], 'LIKE', '%' . $keresett . '%');
+        $order = ['_xx.nev' => 'ASC'];
         $a = $this->alias;
-        $q = $this->_em->createQuery('SELECT ' . $a . '.id,' . $a . '.nev, ' . $a . '.irszam, ' . $a . '.varos, ' . $a . '.utca, ' . $a . '.hazszam,'
+        $q = $this->_em->createQuery(
+            'SELECT ' . $a . '.id,' . $a . '.nev, ' . $a . '.irszam, ' . $a . '.varos, ' . $a . '.utca, ' . $a . '.hazszam,'
             . $a . '.szamlatipus, ' . $a . '.email'
             . ' FROM ' . $this->entityname . ' ' . $a
             . $this->getFilterString($filter)
-            . $this->getOrderString($order));
+            . $this->getOrderString($order)
+        );
         $q->setParameters($this->getQueryParameters($filter));
         $res = $q->getScalarResult();
         return $res;
     }
 
-    public function getByCimkek($cimkefilter) {
-        $partnerkodok = array();
+    public function getByCimkek($cimkefilter)
+    {
+        $partnerkodok = [];
         if ($cimkefilter) {
             if (is_array($cimkefilter)) {
                 $cimkekodok = implode(',', $cimkefilter);
-            }
-            else {
+            } else {
                 $cimkekodok = $cimkefilter;
             }
             if ($cimkekodok) {
-
                 $rsm = new ResultSetMapping();
                 $rsm->addScalarResult('partner_id', 'id');
                 $sql = 'SELECT pc.partner_id'
                     . ' FROM partner_cimkek pc'
-                    . ' WHERE pc.cimketorzs_id IN (' . $cimkekodok .')';
+                    . ' WHERE pc.cimketorzs_id IN (' . $cimkekodok . ')';
                 $q = $this->_em->createNativeQuery($sql, $rsm);
                 $res = $q->getScalarResult();
                 foreach ($res as $sor) {
                     $partnerkodok[] = $sor['id'];
                 }
-
             }
         }
         return $partnerkodok;
     }
 
-	public function countByEmail($email) {
-		$filter = new \mkwhelpers\FilterDescriptor();
+    public function countByEmail($email)
+    {
+        $filter = new \mkwhelpers\FilterDescriptor();
         $filter
             ->addFilter('email', '=', $email)
             ->addFilter('vendeg', '=', false);
-		return $this->getCount($filter);
-	}
+        return $this->getCount($filter);
+    }
 
-	public function findByUserPass($user, $pass) {
+    public function findByUserPass($user, $pass)
+    {
         $filter = new \mkwhelpers\FilterDescriptor();
         $filter
             ->addFilter('email', '=', $user)
             ->addFilter('jelszo', '=', sha1(strtoupper(md5($pass)) . \mkw\store::getSalt()));
-        return $this->getAll($filter, array());
-	}
+        \mkw\store::writelog(print_r($filter->getArray(), true));
+        return $this->getAll($filter, []);
+    }
 
-	public function findVendegByEmail($email) {
+    public function findVendegByEmail($email)
+    {
         $filter = new \mkwhelpers\FilterDescriptor();
         $filter
             ->addFilter('email', '=', $email)
             ->addFilter('vendeg', '=', true);
-		return $this->getAll($filter, array());
-	}
+        return $this->getAll($filter, []);
+    }
 
-    public function findByEmail($email) {
+    public function findByEmail($email)
+    {
         $filter = new \mkwhelpers\FilterDescriptor();
         $filter
             ->addFilter('email', '=', $email);
-        return $this->getAll($filter, array());
+        return $this->getAll($filter, []);
     }
 
-    public function findByIdSessionid($id, $sessionid) {
+    public function findByIdSessionid($id, $sessionid)
+    {
         $filter = new \mkwhelpers\FilterDescriptor();
         $filter
             ->addFilter('id', '=', $id)
             ->addFilter('sessionid', '=', $sessionid);
-		return $this->getAll($filter, array());
-	}
+        return $this->getAll($filter, []);
+    }
 
-    public function findNemVendegByEmail($email) {
+    public function findNemVendegByEmail($email)
+    {
         $filter = new \mkwhelpers\FilterDescriptor();
         $filter
             ->addFilter('email', '=', $email)
             ->addFilter('vendeg', '=', false);
-		return $this->getAll($filter, array());
+        return $this->getAll($filter, []);
     }
 
-    public function checkloggedin() {
+    public function checkloggedin()
+    {
         if (isset(\mkw\store::getMainSession()->pk)) {
             $users = $this->findByIdSessionid(\mkw\store::getMainSession()->pk, \Zend_Session::getId());
             return count($users) == 1;
@@ -213,34 +235,38 @@ class PartnerRepository extends \mkwhelpers\Repository {
         return false;
     }
 
-    public function getLoggedInUser() {
+    public function getLoggedInUser()
+    {
         if ($this->checkloggedin()) {
             return $this->find(\mkw\store::getMainSession()->pk);
         }
         return null;
     }
 
-	public function checkloggedinUKPartner() {
-		if (isset(\mkw\store::getMainSession()->ukpartner)) {
-			$users = $this->findByIdSessionid(\mkw\store::getMainSession()->ukpartner, \Zend_Session::getId());
-			return count($users) == 1;
-		}
-		return false;
-	}
+    public function checkloggedinUKPartner()
+    {
+        if (isset(\mkw\store::getMainSession()->ukpartner)) {
+            $users = $this->findByIdSessionid(\mkw\store::getMainSession()->ukpartner, \Zend_Session::getId());
+            return count($users) == 1;
+        }
+        return false;
+    }
 
-	public function getLoggedInUKPartner() {
-		if ($this->checkloggedinUKPartner()) {
-			return $this->find(\mkw\store::getMainSession()->ukpartner);
-		}
-		return null;
-	}
+    public function getLoggedInUKPartner()
+    {
+        if ($this->checkloggedinUKPartner()) {
+            return $this->find(\mkw\store::getMainSession()->ukpartner);
+        }
+        return null;
+    }
 
-	public function checkAnonym($partnerid) {
+    public function checkAnonym($partnerid)
+    {
         $res = 0;
         $partner = $this->find($partnerid);
         if ($partner) {
             $filter = new \mkwhelpers\FilterDescriptor();
-            $filter->addFilter('bizonylattipus_id', null, array('szamla', 'esetiszamla'));
+            $filter->addFilter('bizonylattipus_id', null, ['szamla', 'esetiszamla']);
             $filter->addFilter('partner_id', '=', $partnerid);
             switch (\mkw\store::getParameter(\mkw\consts::SzamlaOrzesAlap, 1)) {
                 case 1:
@@ -256,21 +282,20 @@ class PartnerRepository extends \mkwhelpers\Repository {
                 $most = date('Y-m-d');
                 if ($datum >= $most) {
                     $res = 0;
-                }
-                else {
+                } else {
                     $res = 1;
                 }
-            }
-            else {
+            } else {
                 $res = 1;
             }
         }
         return $res;
     }
 
-    public function doAnonym($partnerid) {
-	    $res = $this->checkAnonym($partnerid);
-	    /** @var \Entities\Partner $partner */
+    public function doAnonym($partnerid)
+    {
+        $res = $this->checkAnonym($partnerid);
+        /** @var \Entities\Partner $partner */
         $partner = $this->find($partnerid);
         if ($partner) {
             if ($res) {
@@ -304,8 +329,7 @@ class PartnerRepository extends \mkwhelpers\Repository {
                     $this->_em->persist($bbt);
                     $this->_em->flush();
                 }
-            }
-            else {
+            } else {
                 $partner->clearGDPRData();
                 $partner->setAnonymizalnikell(true);
                 $partner->setAnonymkeresdatum(date(\mkw\store::$SQLDateFormat));
