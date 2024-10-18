9e3fab62b54bd63b88af00c1c419a61d6433efa9
law909 <balint.lovey@gmail.com>
2024-10-18 14:13:34 +0200

    Fix WC API request URL by using Termek's WCID in TermekValtozat.

--- Entities/TermekValtozat.php
+++ Entities/TermekValtozat.php
@@ -1380,9 +1380,9 @@ class TermekValtozat
             ];
             $wc = store::getWcClient();
             try {
-                \mkw\store::writelog('products/' . $this->getTermek()?->getWcid() . '/variations/' . $this->getWcid());
                 $result = $wc->put('products/' . $this->getTermek()?->getWcid() . '/variations/' . $this->getWcid(), $variation);
             } catch (HttpClientException $e) {
+                \mkw\store::writelog('products/' . $this->getTermek()?->getWcid() . '/variations/' . $this->getWcid());
                 \mkw\store::writelog($this->getId() . ':TermekValtozat->sendKeszletToWC():HIBA: ' . $e->getResponse()->getBody());
             }
         }
