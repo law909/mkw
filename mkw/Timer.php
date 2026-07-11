8d91138e167302d9a2889753f22f29a5969e1194
law909 <balint.lovey@gmail.com>
2026-07-11 19:59:39 +0200

    Refactor `Timer` class for consistent formatting and improve PHPStan analysis level to 2.

--- mkw/Timer.php
+++ mkw/Timer.php
@@ -1,75 +1,83 @@
 <?php
+
 namespace mkw;
 
 
-class Timer {
+class Timer
+{
 
-	private static $starttimes = array();
-	private static $stoptimes = array();
-	private static $runtimes = array();
-        private static $paused = array();
+    private static $starttimes = [];
+    private static $stoptimes = [];
+    private static $runtimes = [];
+    private static $paused = [];
 
 
-	public static function start($timername) {
-		$mtime = microtime();
-		$mtime = explode(' ',$mtime);
-                if (!array_key_exists($timername, self::$paused)) {
-                    self::$runtimes[$timername] = 0;
-                }
-                unset(self::$paused[$timername]);
-		self::$starttimes[$timername] = $mtime[1] + $mtime[0];
-	}
-
-	public static function stop($timername) {
-		$mtime = microtime();
-		$mtime = explode(' ',$mtime);
-		self::$stoptimes[$timername] = $mtime[1] + $mtime[0];
-		self::$runtimes[$timername] += self::$stoptimes[$timername] - self::$starttimes[$timername];
-		return self::$runtimes[$timername];
-	}
-
-        public static function pause($timername) {
-                $mtime = microtime();
-		$mtime = explode(' ',$mtime);
-                self::$stoptimes[$timername] = $mtime[1] + $mtime[0];
-		self::$runtimes[$timername] += self::$stoptimes[$timername] - self::$starttimes[$timername];
-                self::$paused[$timername] = true;
-                return self::$runtimes[$timername];
+    public static function start($timername)
+    {
+        $mtime = microtime();
+        $mtime = explode(' ', $mtime);
+        if (!array_key_exists($timername, self::$paused)) {
+            self::$runtimes[$timername] = 0;
         }
+        unset(self::$paused[$timername]);
+        self::$starttimes[$timername] = $mtime[1] . $mtime[0];
+    }
 
-	public static function continueTimer($timername) {
-		if (array_key_exists($timername, self::$starttimes)) {
-			self::stop($timername);
-			unset(self::$starttimes[$timername]);
-			unset(self::$stoptimes[$timername]);
-		}
-		else {
-			self::start($timername);
-		}
-	}
+    public static function stop($timername)
+    {
+        $mtime = microtime();
+        $mtime = explode(' ', $mtime);
+        self::$stoptimes[$timername] = $mtime[1] . $mtime[0];
+        self::$runtimes[$timername] += self::$stoptimes[$timername] - self::$starttimes[$timername];
+        return self::$runtimes[$timername];
+    }
 
-	public static function clearTimer($timername) {
-		unset(self::$starttimes[$timername]);
-		unset(self::$stoptimes[$timername]);
-		unset(self::$runtimes[$timername]);
-	}
+    public static function pause($timername)
+    {
+        $mtime = microtime();
+        $mtime = explode(' ', $mtime);
+        self::$stoptimes[$timername] = $mtime[1] . $mtime[0];
+        self::$runtimes[$timername] += self::$stoptimes[$timername] - self::$starttimes[$timername];
+        self::$paused[$timername] = true;
+        return self::$runtimes[$timername];
+    }
 
-	public static function getRuntime($timername, $clear = true) {
-		$result = self::$runtimes[$timername];
-		if ($clear) {
-			unset(self::$runtimes[$timername]);
-		}
-		return $result;
-	}
+    public static function continueTimer($timername)
+    {
+        if (array_key_exists($timername, self::$starttimes)) {
+            self::stop($timername);
+            unset(self::$starttimes[$timername]);
+            unset(self::$stoptimes[$timername]);
+        } else {
+            self::start($timername);
+        }
+    }
 
-    public static function getRuntimeStrings() {
-        $r = array();
-        foreach(self::$runtimes as $k => $v) {
+    public static function clearTimer($timername)
+    {
+        unset(self::$starttimes[$timername]);
+        unset(self::$stoptimes[$timername]);
+        unset(self::$runtimes[$timername]);
+    }
+
+    public static function getRuntime($timername, $clear = true)
+    {
+        $result = self::$runtimes[$timername];
+        if ($clear) {
+            unset(self::$runtimes[$timername]);
+        }
+        return $result;
+    }
+
+    public static function getRuntimeStrings()
+    {
+        $r = [];
+        foreach (self::$runtimes as $k => $v) {
             $r[] = $k . ': ' . $v;
         }
-        self::$starttimes = array();
-        self::$stoptimes = array();
-        self::$runtimes = array();
+        self::$starttimes = [];
+        self::$stoptimes = [];
+        self::$runtimes = [];
         return implode("\n", $r);
     }
 }
