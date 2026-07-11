af4706e343e4b4829d71c17b45f944d17b7ce047
law909 <balint.lovey@gmail.com>
2026-07-11 05:17:26 +0200

    Standardize formatting in `leltarfej.js` by fixing indentation and spacing for improved readability.

--- js/admin/default/leltarfej.js
+++ js/admin/default/leltarfej.js
@@ -1,42 +1,42 @@
-$(document).ready(function(){
-	var dialogcenter = $('#dialogcenter');
-	var leltarfej = {
-			container: '#mattkarb',
-			viewUrl: '/admin/leltarfej/getkarb',
-			newWindowUrl: '/admin/leltarfej/viewkarb',
-			saveUrl: '/admin/leltarfej/save',
-			beforeShow: function() {
-			},
-			beforeSerialize: function(form,opt) {
-			},
-			onSubmit: function() {
-				$('#messagecenter')
-					.html('A mentés sikerült.')
-					.hide()
-					.addClass('matt-messagecenter ui-widget ui-state-highlight')
-					.one('click',messagecenterclick)
-					.slideToggle('slow');
-				//setTimeout('$("#messagecenter").unbind(messagecenterclick).slideToggle("slow");',5000);
-			}
-		};
+$(document).ready(function () {
+    var dialogcenter = $('#dialogcenter');
+    var leltarfej = {
+        container: '#mattkarb',
+        viewUrl: '/admin/leltarfej/getkarb',
+        newWindowUrl: '/admin/leltarfej/viewkarb',
+        saveUrl: '/admin/leltarfej/save',
+        beforeShow: function () {
+        },
+        beforeSerialize: function (form, opt) {
+        },
+        onSubmit: function () {
+            $('#messagecenter')
+                .html('A mentés sikerült.')
+                .hide()
+                .addClass('matt-messagecenter ui-widget ui-state-highlight')
+                .one('click', messagecenterclick)
+                .slideToggle('slow');
+            //setTimeout('$("#messagecenter").unbind(messagecenterclick).slideToggle("slow");',5000);
+        }
+    };
 
-	if ($.fn.mattable) {
-		$('#mattable-select').mattable({
-			name: 'leltarfej',
-			filter: {
-				fields: [
+    if ($.fn.mattable) {
+        $('#mattable-select').mattable({
+            name: 'leltarfej',
+            filter: {
+                fields: [
                     '#nevfilter',
                     '#raktarfilter'
                 ]
-			},
-			tablebody: {
-				url: '/admin/leltarfej/getlistbody',
-                onStyle: function() {
-				    $('.js-export, .js-import, .js-zar').button();
+            },
+            tablebody: {
+                url: '/admin/leltarfej/getlistbody',
+                onStyle: function () {
+                    $('.js-export, .js-import, .js-zar').button();
                 }
-			},
-			karb: leltarfej
-		});
+            },
+            karb: leltarfej
+        });
         var $zarasdatumedit = $('#DatumEdit');
         if ($zarasdatumedit) {
             $zarasdatumedit.datepicker($.datepicker.regional['hu']);
@@ -44,7 +44,7 @@ $(document).ready(function(){
             $zarasdatumedit.datepicker('setDate', $zarasdatumedit.attr('data-datum'));
         }
 
-        $('#mattable-body').on('click','.js-zar', function(e) {
+        $('#mattable-body').on('click', '.js-zar', function (e) {
             var $this = $(this);
             e.preventDefault();
             $('#zarasdatumform').dialog({
@@ -52,7 +52,7 @@ $(document).ready(function(){
                 height: 140,
                 modal: true,
                 buttons: {
-                    'OK': function() {
+                    'OK': function () {
                         var dial = $(this),
                             tol = $('#DatumEdit').datepicker('getDate');
                         tol = tol.getFullYear() + '.' + (tol.getMonth() + 1) + '.' + tol.getDate();
@@ -63,13 +63,13 @@ $(document).ready(function(){
                                 datum: tol,
                                 leltarid: $this.data('leltarfejid')
                             },
-                            success: function() {
+                            success: function () {
                                 $('.mattable-tablerefresh').click();
                                 dial.dialog('close');
                             }
                         });
                     },
-                    'Mégsem': function() {
+                    'Mégsem': function () {
                         $('.mattable-tablerefresh').click();
                         $(this).dialog('close');
                     }
@@ -77,13 +77,12 @@ $(document).ready(function(){
             });
         });
 
-		$('.js-maincheckbox').change(function(){
-			$('.js-egyedcheckbox').prop('checked',$(this).prop('checked'));
-		});
-	}
-	else {
-		if ($.fn.mattkarb) {
-			$('#mattkarb').mattkarb($.extend({},partner,{independent:true}));
-		}
-	}
+        $('.js-maincheckbox').change(function () {
+            $('.js-egyedcheckbox').prop('checked', $(this).prop('checked'));
+        });
+    } else {
+        if ($.fn.mattkarb) {
+            $('#mattkarb').mattkarb($.extend({}, leltarfej, {independent: true}));
+        }
+    }
 });
