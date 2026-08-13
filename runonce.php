456b929b7c794f5bbd1ae3c6d69c723f194dad2e
law909 <balint.lovey@gmail.com>
2026-08-13 02:44:36 +0200

    Add logic to reset "foglal" status for non-reservation setups and update DBVersion to 0119.

--- runonce.php
+++ runonce.php
@@ -1448,6 +1448,15 @@ if ($DBVersion < '0118') {
         \mkw\store::setParameter(\mkw\consts::DBVersion, '0118');
     }
 }
+
+if ($DBVersion < '0119') {
+    // foglalás nélküli telepítésen a státusz foglal jelölője nem hat semmire (a karbantartó is elrejti),
+    // a benne maradt 1-esek viszont azonnal foglalni kezdenének, ha a setup.foglalas bekapcsolódik
+    if (!\mkw\store::isFoglalas()) {
+        \mkw\store::getEm()->getConnection()->executeStatement('UPDATE bizonylatstatusz SET foglal = 0');
+    }
+    \mkw\store::setParameter(\mkw\consts::DBVersion, '0119');
+}
 /**
  * ures partner nevbe betenni vezeteknev+keresztnevet
  * partner nevben cserelni dupla es tripla szokozoket szokozre
