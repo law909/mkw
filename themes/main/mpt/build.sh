7e42e49cc83950a9a56dbbcd3a80c70738ecf72d
law909 <balint.lovey@gmail.com>
2026-09-02 14:20:26 +0200

    Remove MPT module including related templates, scripts, and Tailwind configuration.

--- themes/main/mpt/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-npx tailwindcss -i input.css -o mpt.css
--- themes/main/mpt/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["../../../tpl/main/mpt/**/*.tpl"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
-
