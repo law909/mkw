ec3f065b73251c32994124b78bf7fadeb31c9da2
law909 <balint.lovey@gmail.com>
2026-09-21 23:33:49 +0200

    feat(mainroute): extend stocklist export route to handle Mugenrace2026 condition

--- mainroute.php
+++ mainroute.php
@@ -250,7 +250,7 @@ if (!\mkw\store::isKatalogus()) {
     $router->map('GET', '/export/orderform', 'exportController#orderformExport', 'orderformexport');
     $router->map('GET', '/export/fcmotostock', 'exportController#fcmotostockExport', 'fcmotostockexport');
     $router->map('GET', '/export/eanstock', 'exportController#eanstockExport', 'eanstockexport');
-    if (\mkw\store::isSuperzoneB2B()) {
+    if (\mkw\store::isSuperzoneB2B() || \mkw\store::isMugenrace2026()) {
         $router->map('GET', '/export/stocklist', 'exportController#stocklistExport', 'stocklistexport');
     }
 }
